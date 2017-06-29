3580188a6fe80c3cf78651eadeb56c1beedf685f
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-06-29 16:03:33 +0530

    Added circular background

--- app/src/main/java/com/mereexams/mereexamscalendar/CaldroidActivity.java
+++ app/src/main/java/com/mereexams/mereexamscalendar/CaldroidActivity.java
@@ -4,9 +4,12 @@ import android.icu.util.Calendar;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.FrameLayout;
 
 import com.roomorama.caldroid.CaldroidFragment;
+
+import java.util.Date;
 
 public class CaldroidActivity extends AppCompatActivity {
 
@@ -26,8 +29,14 @@ public class CaldroidActivity extends AppCompatActivity {
         CaldroidFragment caldroidFragment = new CaldroidFragment();
         Bundle args = new Bundle();
         args.putInt(CaldroidFragment.MONTH, 6);
-        args.putInt(CaldroidFragment.YEAR, 2016);
+        args.putInt(CaldroidFragment.YEAR, 2017);
         caldroidFragment.setArguments(args);
+
+        Date today = new Date(2017 - 1900, 5, 29);
+        Log.d("Today", today.toString());
+
+        caldroidFragment.setBackgroundDrawableForDate(getResources().getDrawable(R.drawable.circle), today);
+        caldroidFragment.refreshView();
 
         FragmentTransaction t = getSupportFragmentManager().beginTransaction();
         t.replace(R.id.framelayout_fragment_container, caldroidFragment);
