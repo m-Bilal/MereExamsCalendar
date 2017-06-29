4967695d3f8c62a5b7229fa95443dbddd01cb07a
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-06-29 13:31:50 +0530

    Added onDateSelectedListener

--- app/src/main/java/com/mereexams/mereexamscalendar/MainActivity.java
+++ app/src/main/java/com/mereexams/mereexamscalendar/MainActivity.java
@@ -1,13 +1,30 @@
 package com.mereexams.mereexamscalendar;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.CalendarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    CalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        calendarView = (CalendarView) findViewById(R.id.calendar);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                Log.d("Calendar", "Date changed");
+                Log.d("Year", year + "");
+                Log.d("Month", month + "");
+                Log.d("Day", dayOfMonth + "");
+            }
+        });
     }
 }
