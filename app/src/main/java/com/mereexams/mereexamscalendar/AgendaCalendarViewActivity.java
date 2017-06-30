f4cf1496bb817f3cae8b68573514cad59ae70020
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-06-30 10:24:56 +0530

    Added agenda calendar view activity

---
+++ app/src/main/java/com/mereexams/mereexamscalendar/AgendaCalendarViewActivity.java
@@ -0,0 +1,13 @@
+package com.mereexams.mereexamscalendar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AgendaCalendarViewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agenda_calendar_view);
+    }
+}
