ed7c5cd403c7d3cab7aa9a5074313cd827f379bc
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-06-30 11:08:39 +0530

    Added AgendaCalendarView demo

--- app/src/main/java/com/mereexams/mereexamscalendar/AgendaCalendarViewActivity.java
+++ app/src/main/java/com/mereexams/mereexamscalendar/AgendaCalendarViewActivity.java
@@ -2,12 +2,54 @@ package com.mereexams.mereexamscalendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.github.tibolte.agendacalendarview.AgendaCalendarView;
+import com.github.tibolte.agendacalendarview.CalendarPickerController;
+import com.github.tibolte.agendacalendarview.models.CalendarEvent;
+import com.github.tibolte.agendacalendarview.models.DayItem;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+import java.util.Locale;
 
 public class AgendaCalendarViewActivity extends AppCompatActivity {
+
+    AgendaCalendarView calendarView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_agenda_calendar_view);
+
+        calendarView = (AgendaCalendarView) findViewById(R.id.agenda_calendar_view);
+
+        Calendar minDate = Calendar.getInstance();
+        Calendar maxDate = Calendar.getInstance();
+
+        minDate.add(Calendar.MONTH, -2);
+        minDate.set(Calendar.DAY_OF_MONTH, 1);
+        maxDate.add(Calendar.YEAR, 1);
+
+        List<CalendarEvent> eventList = new ArrayList<>();
+        //mockList(eventList);
+
+        calendarView.init(eventList, minDate, maxDate, Locale.getDefault(), new CalendarPickerController() {
+            @Override
+            public void onDaySelected(DayItem dayItem) {
+                Log.i("Day", dayItem.toString());
+            }
+
+            @Override
+            public void onEventSelected(CalendarEvent event) {
+
+            }
+
+            @Override
+            public void onScrollToDate(Calendar calendar) {
+
+            }
+        });
     }
 }
