7571902c9c251a871472dc496340add4ab147223
Mohd Bilal <mohd.bilal1996@gmail.com>
2017-06-30 12:36:55 +0530

    Added events

--- app/src/main/java/com/mereexams/mereexamscalendar/AgendaCalendarViewActivity.java
+++ app/src/main/java/com/mereexams/mereexamscalendar/AgendaCalendarViewActivity.java
@@ -1,16 +1,22 @@
 package com.mereexams.mereexamscalendar;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 
 import com.github.tibolte.agendacalendarview.AgendaCalendarView;
 import com.github.tibolte.agendacalendarview.CalendarPickerController;
+import com.github.tibolte.agendacalendarview.models.BaseCalendarEvent;
 import com.github.tibolte.agendacalendarview.models.CalendarEvent;
 import com.github.tibolte.agendacalendarview.models.DayItem;
+import com.github.tibolte.agendacalendarview.models.WeekItem;
+import com.mereexams.mereexamscalendar.Models.MyCalendarEvent;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 import java.util.Locale;
 
@@ -33,7 +39,9 @@ public class AgendaCalendarViewActivity extends AppCompatActivity {
         maxDate.add(Calendar.YEAR, 1);
 
         List<CalendarEvent> eventList = new ArrayList<>();
-        //mockList(eventList);
+        mockList(eventList);
+
+        Log.i("Events size", eventList.size() + "");
 
         calendarView.init(eventList, minDate, maxDate, Locale.getDefault(), new CalendarPickerController() {
             @Override
@@ -52,4 +60,29 @@ public class AgendaCalendarViewActivity extends AppCompatActivity {
             }
         });
     }
+
+    private void mockList(List<CalendarEvent> eventList) {
+        Calendar startTime1 = Calendar.getInstance();
+        Calendar endTime1 = Calendar.getInstance();
+        endTime1.add(Calendar.MONTH, 1);
+        BaseCalendarEvent event1 = new BaseCalendarEvent("Thibault travels in Iceland", "A wonderful journey!", "Iceland",
+                ContextCompat.getColor(this, R.color.colorPrimary), startTime1, endTime1, true);
+        eventList.add(event1);
+
+        Calendar startTime2 = Calendar.getInstance();
+        startTime2.add(Calendar.DAY_OF_YEAR, 1);
+        Calendar endTime2 = Calendar.getInstance();
+        endTime2.add(Calendar.DAY_OF_YEAR, 3);
+        BaseCalendarEvent event2 = new BaseCalendarEvent("Visit to Dalvík", "A beautiful small town", "Dalvík",
+                ContextCompat.getColor(this, R.color.colorAccent), startTime2, endTime2, true);
+        eventList.add(event2);
+
+        Calendar startTime3 = Calendar.getInstance();
+        Calendar endTime3 = Calendar.getInstance();
+        Log.i("Month", endTime3.get(Calendar.MONTH) + "");
+        BaseCalendarEvent event3 = new BaseCalendarEvent("My Own Event", "My description", "Temple of Thought",
+                ContextCompat.getColor(this, R.color.event_color), startTime3, endTime3, true);
+        eventList.add(event3);
+    }
+
 }
